a4cade598d4257107646e5554fde5810d457d27e
garima <garimamangalclg@gmail.com>
2024-07-26 20:23:31 +0530

    fix: responsiveness mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,11 +36,11 @@ export default function Home() {
       <Navbar />
       <div className="bg-product_landing_page w-full h-screen bg-cover bg-no-repeat flex items-end pb-10 justify-center relative">
         <Toaster />
-        <button className="rounded-full bg-red-500 h-[6rem] w-[6rem] fixed bottom-[5%] right-[3%] font-bold text-white ring-red-500 ring-[1px] p-2">
+        <button className="md:text-base text-xs rounded-full bg-red-500 md:h-[6rem] md:w-[6rem] w-[4.5rem] h-[4.5rem] fixed bottom-[5%] right-[3%] font-bold text-white ring-red-500 ring-[1px] p-2 z-10">
           Go to Cafe Menu
         </button>
         <div className="lg:w-[30rem] lg:h-[30rem] w-[23rem] h-[23rem] rounded-full  bg-slate-500/30 backdrop-blur-md flex items-center justify-center flex-col relative">
-          <h1 className="lg:text-[4.5rem] md:text-[3.5rem] font-bold text-blue-700 font-serif">
+          <h1 className="lg:text-[4.5rem] text-[3.5rem] font-bold text-blue-700 font-serif">
             Cafe Street
           </h1>
           <p className="lg:text-[1.25rem] md:text-[1rem] font-bold text-white text-center">
@@ -51,12 +51,16 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <section className="lg:py-[8rem] py-[5rem] lg:px-[5rem] px-[2rem] flex items-center justify-center space-x-[5rem] max-w-full bg-gradient-to-b from-red-400/20 to-blue-200">
-        <div className="flex items-start justify-center flex-col space-y-4 w-[40%]">
-          <h1 className="lg:text-[2.5rem] text-[2rem] text-blue-700 ">
+      <section className="lg:py-[8rem] md:py-[5rem] py-[3rem] lg:px-[5rem] px-[2rem] flex md:flex-row flex-col-reverse items-center justify-center md:space-x-[5rem] max-w-full bg-gradient-to-b from-red-400/20 to-blue-200">
+        <div className="flex md:items-start items-center justify-center flex-col space-y-4 md:w-[40%]">
+          <h1 className="lg:text-[2.5rem] text-[2rem] text-blue-700 md:text-left text-center">
             Fastest way to set up your Menu
           </h1>
-          <p className="text-slate-500 lg:w-[80%] w-[90%] lg:text-[1rem] text-[0.9rem]">
+          <img
+            src="/cafe/illus_website.png"
+            className="md:w-[30%] w-[80%] md:mb-0 mb-5 flex md:hidden"
+          ></img>
+          <p className="text-slate-500 lg:w-[80%] md:w-[90%] lg:text-[1rem] text-[0.9rem] md:text-left text-center">
             With{" "}
             <span className="italic font-semibold text-slate-600">
               Cafe Street
@@ -64,17 +68,20 @@ export default function Home() {
             you can streamline your experience of managing a cafe.
           </p>
         </div>
-        <img src="/cafe/illus_website.png" className="w-[30%]"></img>
+        <img
+          src="/cafe/illus_website.png"
+          className="md:w-[30%] w-[80%] md:mb-0 mb-5 md:flex hidden"
+        ></img>
       </section>
-      <section className="flex items-center justify-center flex-col bg-gradient-to-b from-blue-200 to-blue-200/60 lg:py-[8rem] py-[5rem] lg:px-[5rem] px-[2rem]">
+      <section className="flex items-center justify-center flex-col bg-gradient-to-b from-blue-200 to-blue-200/60 lg:py-[8rem] md:py-[5rem] py-[3rem] lg:px-[5rem] px-[2rem]">
         <h1 className="lg:text-[2.5rem] text-[2rem] text-blue-700 mb-4">
           Features
         </h1>
-        <p className=" lg:w-[20%] w-[40%] lg:text-[1rem] text-[0.9rem] text-center text-slate-500 mb-[5rem]">
+        <p className=" lg:w-[20%] md:w-[40%] lg:text-[1rem] text-[0.9rem] text-center text-slate-500 mb-[5rem]">
           An app to ease the heat off cafe owners and make managing your cafe a
           breeze
         </p>
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 lg:gap-[8rem] gap-[6rem] w-full px-[4rem] place-content-center">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 lg:gap-[8rem] md:gap-[6rem] gap-[2rem] w-full md:px-[4rem] px-[2rem] place-content-center">
           <FeatItem
             img="/cafe/tablet.png"
             heading="Digital Menu"
@@ -90,22 +97,21 @@ export default function Home() {
             heading="Online Payment"
             desc="Streamline transactions with secure and convenient online payment options."
           ></FeatItem>
-          <FeatItem
-            img="/cafe/online-payment.png"
-            heading="Online Payment"
-            desc="Streamline transactions with secure and convenient online payment options."
-          ></FeatItem>
         </div>
       </section>
       <section className="flex items-center justify-center flex-col bg-gradient-to-b from-blue-200/60 to-white lg:py-[8rem] py-[5rem] lg:px-[5rem] px-[2rem]">
         <h1 className="lg:text-[2.5rem] text-[2rem] text-blue-700 mb-4">
           Contact Us
         </h1>
+        <img
+          src="/cafe/cafe_contact.png"
+          className="w-[80%] flex md:hidden"
+        ></img>
         <p className=" text-center text-slate-500 mb-[2rem] lg:text-[1rem] text-[0.9rem]">
           We are here to help you with seamless onboarding.
         </p>
-        <div className="w-full lg:px-[4rem] flex items-center justify-around lg:space-x-[10rem] space-x-[5rem]">
-          <div className="lg:p-[2rem] p-[1.25rem] shadow-2xl rounded-xl bg-white items-center justify-center flex flex-col lg:w-[30%]">
+        <div className="w-full lg:px-[4rem] md:px-0 px-[1rem] flex md:flex-row flex-col items-center justify-around lg:space-x-[10rem] space-x-[5rem]">
+          <div className="lg:p-[2rem] md:p-[1.25rem] p-[1.5rem] shadow-2xl rounded-xl bg-white items-center justify-center flex flex-col lg:w-[30%] w-full">
             <p className="mb-[2rem] font-bold text-blue-700 text-lg">
               Get In Touch
             </p>
@@ -154,7 +160,10 @@ export default function Home() {
               </button>
             </form>
           </div>
-          <img src="/cafe/cafe_contact.png" className="w-[50%]"></img>
+          <img
+            src="/cafe/cafe_contact.png"
+            className="w-[50%] md:flex hidden"
+          ></img>
         </div>
       </section>
       <footer className="bg-zinc-400 px-[3rem] py-[0.75rem] w-full flex items-center justify-between">
@@ -175,11 +184,13 @@ export default function Home() {
 
 function FeatItem({ img, heading, desc }: CafeFeature) {
   return (
-    <div className="rounded-2xl px-[1.5rem] py-[1rem] shadow-2xl flex flex-col items-center lg:space-y-5 space-y-3 ring-[1px] bg-white/50 hover:-translate-y-2 backdrop-blur-sm backdrop-filter">
+    <div className="rounded-2xl sm:px-[1.5rem] px-[1rem] py-[1rem] shadow-2xl flex flex-col items-center lg:space-y-5 space-y-3 ring-[1px] bg-white/50 hover:-translate-y-2 backdrop-blur-sm backdrop-filter">
       <div className="rounded-full bg-blue-300/30 p-3 mt-2">
         <img src={img}></img>
       </div>
-      <h1 className="lg:text-xl text-lg font-bold text-blue-700">{heading}</h1>
+      <h1 className="lg:text-xl text-lg font-bold text-blue-700 text-center">
+        {heading}
+      </h1>
       <p className="text-center lg:text-base text-sm">{desc}</p>
     </div>
   );
--- src/components/cafe/Navbar.tsx
+++ src/components/cafe/Navbar.tsx
@@ -1,18 +1,17 @@
 export default function Navbar() {
   return (
-    <nav className="w-full fixed flex items-center justify-between bg-red-800/40 top-0 backdrop-blur-[8px] backdrop-filter px-[2rem] py-[1.5rem] z-10">
+    <nav className="w-full fixed flex items-center justify-between bg-red-800/40 top-0 backdrop-blur-[8px] backdrop-filter md:px-[2rem] px-[1rem] py-[1.5rem] z-10">
       <img
         src="/logo_coffe.svg"
-        className="bg-secondary/20 p-1 rounded-md"
+        className="bg-secondary/20 p-1 rounded-md md:scale-100 scale-75"
       ></img>
-      <div className="flex items-center justify-center space-x-[1rem]">
-        <button className="px-[1.5rem] py-2 bg-blue-700 rounded-lg text-white font-bold hover:bg-blue-800">
+      <div className="flex items-center justify-center md:space-x-[1rem] space-x-[0.5rem]">
+        <button className="px-[1.5rem] py-2 bg-blue-700 rounded-lg text-white font-bold hover:bg-blue-800 md:text-base text-xs">
           SignIn
         </button>
-        <button className="text-blue-700 rounded-lg border-2 border-blue-700 px-[1.25rem] py-[6px] font-bold hover:bg-blue-800 hover:text-white hover:border-blue-700/80">
+        <button className="text-blue-700 rounded-lg border-2 border-blue-700 px-[1.25rem] py-[6px] font-bold hover:bg-blue-800 hover:text-white hover:border-blue-700/80 md:text-base text-xs">
           Signup
         </button>
-        
       </div>
     </nav>
   );
